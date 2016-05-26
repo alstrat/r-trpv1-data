4b8e722ea5ffccffb33d6e4fe50a078b777186d9
OleksaStrat <ostratiyevskyy@gmail.com>
2016-05-25 23:00:08 -0700

    mvp + playing with anova. no grpups bug ixed for summary

--- trpv1.R
+++ trpv1.R
@@ -16,10 +16,12 @@ logVar <- function(string, var) {
 
 ########################### Read XLSX ###########################
 get_first_data_length <- function(data_sheet){
+
     options(warn=-1)
     first_row <- readColumns(data_sheet, startRow = 2, endRow = 2, startColumn = 1, endColumn = 100, header=FALSE)
-    first_blank_index <- min(which(first_row == ""))
+    first_blank_index <- min(which((first_row == "") | is.na(first_row)))
     options(warn=0)
+    print(first_blank_index)
     return((first_blank_index - 1))
 }
 
@@ -34,7 +36,6 @@ get_sheet_first_data <- function(file_name, sheet_number, dataType){
             sheetIndex = sheet_number, 
             colIndex = (1:first_data_length),
                   colClasses=rep(dataType, first_data_length))
-
     data <- data.matrix(raw[1:first_data_length])
     return(data)
 }
@@ -42,15 +43,13 @@ get_sheet_first_data <- function(file_name, sheet_number, dataType){
 
 # Process 1 sheet of input spreadsheet. TODO: rename
 get_input_sheet_data <- function(file_name, sheet_number){
+
     raw_data <- get_sheet_first_data(file_name, sheet_number, "numeric")
-    
     bg_index <- ncol(raw_data)
     bg <- raw_data[,bg_index]
     time <- raw_data[,1]
     trpv_data <- raw_data[,2:(bg_index-1)]
-    
     processed_data <- apply(trpv_data, 2, process_trpv_col, bg=bg, time=time)
-
     return(processed_data)
 }
 
@@ -112,12 +111,7 @@ process_trpv_col <- function(in_trpv_column, bg, time) {
     # take all and divide by mean from baseline period = x/mean(x[1:noise_index]), 
     # standard deviation currently not used
     mean_noaction <- mean(cleansed_trpv_column[1:noise_index])
-    # sd_noaction <- sd(cleansed_trpv_column[1:120]) 
-    # normalized_trpv <- vapply(cleansed_trpv_column, function(x) (abs(x) - sd_noaction)/mean_noaction * sign(x), 0)
     normalized_trpv <- vapply(cleansed_trpv_column, function(x) x/mean_noaction, 0)
-    
-    # normalized_trpv <- vapply(cleansed_trpv_column, function(x) (x - min(x))/(max(x) - min(x)), 0)
-    
     return(normalized_trpv)
 }
 
@@ -131,8 +125,8 @@ process_trpv1_input_file <- function(in_file_name, draw_plot_flag) {
     # build output files basename based on input filename
     #   add extentions and suffixes
     #   output in out/ subfolder
-    file_name <- paste("in/", in_file_name, sep="")
-    out_base_name <- paste("out/", (sub(".xlsx", "", in_file_name)), sep="")
+    file_name <- paste(EXPERIMENT_FILES_DIR, "/", in_file_name, sep="")
+    out_base_name <- paste(OUTPUT_FILES_DIR, "/", (sub(".xlsx", "", in_file_name)), sep="")
     
     # logging steps to console for progress monitoring
     logVar("[START] Processing", file_name)
@@ -142,23 +136,15 @@ process_trpv1_input_file <- function(in_file_name, draw_plot_flag) {
     aktph <- get_input_sheet_data(file_name, AKTPH_INDEX)
     trpv1 <- get_input_sheet_data(file_name, TRPV_INDEX)
 
+    input_data_list <- list(aktph, trpv1)
+    names(input_data_list) <- DATA_TYPE_HEADERS[AKTPH_INDEX: TRPV_INDEX]
+
     # optional, draw plot into PNG
     if (draw_plot_flag == 1 ) {
         plot_initial(out_base_name, trpv1, aktph)
     }
-   
-    # dump resulting tables into excel
-    output_file_name = paste(out_base_name, "_out.xlsx", sep="")
-    
-    logVar("    [START] Dumping results into", output_file_name)
-    
-    write.xlsx(aktph, file=output_file_name, sheetName="processed_aktph")
-    write.xlsx(trpv1, file=output_file_name, sheetName="processed_trpv1", append=TRUE)
-    
-    logVar("    [END] Dumping results into", output_file_name)
-    logVar("[END] Processing", file_name)
-    
-    return(output_file_name)
+
+    return(input_data_list)
 }
 
 
@@ -184,20 +170,15 @@ plot_data <- function(data, title) {
     data_plot <- ggplot(d, aes(Var1,value, col=Var2))+ 
         geom_line(size=1.2)+
         labs(x="t", y="cells", title=title) +
-        coord_cartesian(xlim=c(100, 360), ylim=c(-1, 3))+
-        scale_x_continuous(breaks=seq(0, 500, 20))+
+        coord_cartesian(xlim=c(0, 140), ylim=c(-1, 3))+
+        scale_x_continuous(breaks=seq(0, 140, 20))+
         scale_y_continuous(breaks=seq(-1, 3, 0.2))
     return(data_plot)
 }
 
-plot_groups_independent <- function(grouped_data, file_name){
-    # aktph_plot <- plot_data(grouped_data[[AKTPH_INDEX]], "aktph")
-    # trpv_plot <- plot_data(grouped_data[[TRPV_INDEX]], "trpv")
-    # trpv_aktph_plot <- plot_data(grouped_data[[TRPV_AKTPH_INDEX]], "trpv_aktph")
-    # plots <- list(aktph_plot, trpv_plot, trpv_aktph_plot)
-    
-    data_names <- names(grouped_data)
-    plots <- lapply(data_names, function(x) plot_data(grouped_data[x], x))
+plot_data_list <- function(data_list, file_name){
+    data_names <- names(data_list)
+    plots <- lapply(data_names, function(x) plot_data(data_list[x], x))
     
     draw_plot_list_into_file(plots, file_name)
 }
@@ -234,10 +215,7 @@ group_against_base_aktph <- function(test_data, base){
 #   column elements from (noise_index + 1) to (TEST_DATA_SIZE * noise_index) considered as sample to choose group
 # TODO: refactor
 get_data_group_aktph <- function(data) {
-    
-    TEST_DATA_MIN <- 140
-    TEST_DATA_MAX <- 180
-    
+
     base<- data[1: noise_index]
     # sample to determine group
     test_data <- data[TEST_DATA_MIN: TEST_DATA_MAX]
@@ -265,9 +243,7 @@ group_against_base_trpv <- function(test_data, base){
 }
 
 get_data_group_trpv <- function(data) {
-    
-    TEST_DATA_MIN <- 120
-    
+
     base<- data[1: noise_index]
     # sample to determine group
     test_data <- data[-(1:TEST_DATA_MIN)]
@@ -298,11 +274,11 @@ get_indices_by_names <- function(data, filter_colnames) {
 
 # read data table from excel tab based on filename and sheet number
 # calculate groups and add group labels to column headers
-group_data <- function(data_file, sheet_num, grouping_function) {
+group_data <- function(data_list, data_file, tab_num, grouping_function) {
 
-    file_base <- sub("_out.xlsx", "", sub("out/", "", data_file))
+    file_base <- sub(".xlsx", "", data_file)
     
-    data <- get_sheet_first_data(data_file, sheet_num, "numeric")[,-1]
+    data <- data_list[[tab_num]]
     data <- append_headers(data, rep(file_base, ncol(data)))
     data_groups <- apply(data, 2, grouping_function)
     grouped_data <- append_headers(data, data_groups)
@@ -324,17 +300,16 @@ get_dependent_data <- function(group_header, grouped_aktph, grouped_trpv){
 
 build_grouped_data <- function(grouped_aktph, grouped_trpv){
 
-    data_type_headers <- c("AKTPH", "TRPV", "TRPV-AKTPH")
     group_headers <- c("HIGH", "FLAT", "LOW")
     
     grouped_data_independent <- list(grouped_aktph, grouped_trpv)
-    independent_headers <- data_type_headers[AKTPH_INDEX: TRPV_INDEX]
+    independent_headers <- DATA_TYPE_HEADERS[AKTPH_INDEX: TRPV_INDEX]
     names(grouped_data_independent) <- independent_headers
 
     grouped_data_dependent <- lapply(group_headers, 
         get_dependent_data, grouped_aktph=grouped_aktph, grouped_trpv=grouped_trpv)
     dependent_headers <- vapply(group_headers, 
-        function(x) paste (data_type_headers[TRPV_AKTPH_INDEX], x, sep="."), "")
+        function(x) paste (DATA_TYPE_HEADERS[TRPV_AKTPH_INDEX], x, sep="."), "")
     names(grouped_data_dependent) <- dependent_headers
     
     grouped_data <- c(grouped_data_independent, grouped_data_dependent)
@@ -352,26 +327,24 @@ dump_into_file <- function(data, file) {
     options(warn=0)
     lapply(data_names, function(x) 
         if (ncol(data[[x]]) > 0) {write.xlsx(data[[x]], file=output_file_name, sheetName=x, append=TRUE)})
+        # write.xlsx(data[[x]], file=output_file_name, sheetName=x, append=TRUE))
     
     logVar("    [END] Dumping results into", output_file_name)
 }
     
 # Group cleansed data from step 1 related to baseline
-grouping <- function(data_file, draw_plot_flag){
+grouping <- function(data_list,  data_file, draw_plot_flag){
 
-    out_base_name <- paste(sub(".xlsx", "", data_file), "_GRP", sep="")
-
-    grouped_aktph <- group_data(data_file, AKTPH_INDEX, get_data_group_aktph)
-    grouped_trpv <- group_data(data_file, TRPV_INDEX, get_data_group_trpv)
-    
+    out_base_name <- paste(OUTPUT_FILES_DIR, "/", sub(".xlsx", "", data_file), "_GRP", sep="")
+    grouped_aktph <- group_data(data_list, data_file, AKTPH_INDEX, get_data_group_aktph)
+    grouped_trpv <- group_data(data_list, data_file, TRPV_INDEX, get_data_group_trpv)
     grouped_data <- build_grouped_data(grouped_aktph, grouped_trpv)
 
     # optional, draw plot into R frame if input parameter draw_plot_flag == 1
     # TODO: unification, layout, PNG
     if (draw_plot_flag == 1 ) {
-        plot_groups_independent(grouped_data, out_base_name)
+        plot_data_list(grouped_data, out_base_name)
     }
-    
     if (DUMP_INTO_XLSX == 1 ) {
         dump_into_file(grouped_data, out_base_name)
     }
@@ -384,8 +357,8 @@ grouping <- function(data_file, draw_plot_flag){
 # TODO: refactor for proper config usages.
 trpv1_file <- function(experiment_file){
 
-    data_file <- process_trpv1_input_file(experiment_file, DRAW_PLOTS)
-    grouped_data <- grouping(data_file, DRAW_PLOTS)
+    data_list <- process_trpv1_input_file(experiment_file, DRAW_PLOTS)
+    grouped_data <- grouping(data_list, experiment_file, DRAW_PLOTS)
     return(grouped_data)
 }
 
@@ -399,35 +372,68 @@ if_vector_to_matrix <- function(v,old_m){
     }
 }
 
+build_summary_max_tab <- function(aktph, trpv) {
+
+    t_aktph <- t(aktph)
+    t_aktph_test_max <- apply(t_aktph, 1, function(x) max(x[TEST_DATA_MIN: TEST_DATA_MAX]))
+    t_aktph_group <- vapply(rownames(t_aktph), function(x) gsub( "\\..*$", "", x ), "")
+    res <- cbind(t_aktph_test_max, t_aktph_group)
+    t_trpv <- t(trpv)
+    t_trpv_max <- apply(t_trpv, 1, function(x) max(x[-(1:TEST_DATA_MIN)]))
+    t_trpv_group <- vapply(rownames(t_trpv), function(x) gsub( "\\..*$", "", x ), "")
+    res <- cbind(res, t_trpv_max)
+    res <- cbind(res, t_trpv_group)
+    return(res)
+}
+
 trpv1_files <- function(){
     
     experiment_files <- list.files(EXPERIMENT_FILES_DIR, pattern=".xlsx")
     grouped_data_list <- lapply(experiment_files, trpv1_file)
     summary <- grouped_data_list[[1]]
-    summary_file_basename <- "out/trvp1_aggregated"
+    summary_file_basename <- paste(OUTPUT_FILES_DIR, "/", "trvp1_aggregated", sep="")
 
     for (grouped_data in grouped_data_list[-1]){
         for (name in names(summary) ){
             summ_data_matrix <- summary[[name]]
             grouped_data_matrix <- grouped_data[[name]]
-            if (ncol(grouped_data_matrix) > 0)  {
-                min_nrow <- min(nrow(summ_data_matrix), nrow(grouped_data_matrix))
-                sum_part <- if_vector_to_matrix(summ_data_matrix[1:min_nrow,], summ_data_matrix)
-                g_part <-  if_vector_to_matrix(grouped_data_matrix[1:min_nrow,], grouped_data_matrix)
-                m <- cbind(sum_part, g_part)
-                m <- m[, order(colnames(m))]
+            if (ncol(grouped_data_matrix) > 1 )  {
+                sum_rows <- nrow(summ_data_matrix)
+                g_rows <-  nrow(grouped_data_matrix)
+                if (sum_rows < 1) {
+                    m <- grouped_data_matrix
+                }else if (g_rows < 1) {
+                    m <- summ_data_matrix
+                } else {
+                    min_nrow <- min(sum_rows, g_rows)
+                    sum_part <- if_vector_to_matrix(summ_data_matrix[1:min_nrow,], summ_data_matrix)
+                    g_part <-  if_vector_to_matrix(grouped_data_matrix[1:min_nrow,], grouped_data_matrix)
+                    m <- cbind(sum_part, g_part)
+                    m <- m[, order(colnames(m))]
+                }
                 summary[[name]] <- m
             }
         }
     }
-
-    dump_into_file(summary, summary_file_basename)
     
     if (DRAW_PLOTS == 1 ) {
-        plot_groups_independent(summary, summary_file_basename)
+        plot_data_list(summary, summary_file_basename)
     }
+
+    summary_max_tab <- build_summary_max_tab(summary[[AKTPH_INDEX]], summary[[TRPV_INDEX]])
+    summary[["SUMMARY"]] <- summary_max_tab
+    dump_into_file(summary, summary_file_basename)
+    return(summary_max_tab)
 }
 
 
 ################################# test run-trvp1.R #########################################
-# trpv1_files()
+# summary <- trpv1_files()
+# x <- as.numeric(summary[,1])
+# y <- as.numeric(summary[,3])
+
+# aov.trpv1 <- aov(y~x)
+# means <- (model.tables(aov.trpv1,"means"))$tables$x
+# a <- cbind(matrix(x), matrix(y), matrix(means))
+# lm.trpv1 <- lm(y~x)
+# a <- cbind(a, predict(lm.trpv1))
